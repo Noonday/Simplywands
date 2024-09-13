68927160d0c4fedd9a686924c87e689cdd42e6df
noonday <xunyzftw@gmail.com>
2024-09-13 14:56:06 +0200

    Updated max size of ByteBufCodecs.list

--- src/main/java/net/bennysmith/simplywands/network/HighlightOresPayload.java
+++ src/main/java/net/bennysmith/simplywands/network/HighlightOresPayload.java
@@ -7,7 +7,6 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 
-
 import java.util.List;
 
 public record HighlightOresPayload(List<BlockPos> orePositions) implements CustomPacketPayload {
@@ -16,7 +15,7 @@ public record HighlightOresPayload(List<BlockPos> orePositions) implements Custo
     public static final Type<HighlightOresPayload> TYPE = new Type<>(ID);
 
     public static final StreamCodec<ByteBuf, HighlightOresPayload> CODEC = StreamCodec.composite(
-            BlockPos.STREAM_CODEC.apply(ByteBufCodecs.list(2048)), // Limit to 2048 positions
+            BlockPos.STREAM_CODEC.apply(ByteBufCodecs.list(8192)), // OLD Limit to 2048 positions
             HighlightOresPayload::orePositions,
             HighlightOresPayload::new
     );
