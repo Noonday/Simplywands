44fdca67386c4c57baee1998260a477d0d43b996
noonday <xunyzftw@gmail.com>
2024-09-15 18:06:44 +0200

    Nicer looking Config file.

--- src/main/java/net/bennysmith/simplywands/Config.java
+++ src/main/java/net/bennysmith/simplywands/Config.java
@@ -9,7 +9,11 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-    // Config for Ore Locator Wand
+    // Ore-Locator Wand group
+    static {
+        BUILDER.push("Ore-Locator Wand");
+    }
+
     private static final ModConfigSpec.IntValue HIGHLIGHT_RADIUS = BUILDER
             .comment("The radius (in blocks) in which ores will be highlighted")
             .comment("Default: 16")
@@ -20,8 +24,11 @@ public class Config {
             .comment("Default: 30000")
             .defineInRange("highlightDurationMs", 30000L, 1000L, 60000L);
 
+    static {
+        BUILDER.pop();
+    }
+
     // Public static fields for easy access
-    public static boolean enableUpdateChecker;
     public static int highlightRadius;
     public static long highlightDurationMs;
 
